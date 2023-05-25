a1c771aca891f727a28973ca5d99525154d46210
athenacats <124017569+athenacats@users.noreply.github.com>
2023-05-25 11:57:29 +0300

    Create lingerie model

---
+++ frontend/src/app/shared/models/lingerie.ts
@@ -0,0 +1,11 @@
+export class Lingerie {
+  id!: string;
+  name!: string;
+  price!: string;
+  tags?: string[];
+  favorite!: boolean;
+  start!: number;
+  imageUrl!: string;
+  description!: string;
+  color!: string;
+}
