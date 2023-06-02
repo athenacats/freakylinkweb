bdf6fa95d4268bbfa155e9989be41febfdd2d9ff
athenacats <124017569+athenacats@users.noreply.github.com>
2023-06-02 14:32:36 +0300

    Add description to page

--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { LingerieService } from 'src/app/services/lingerie.service';
 import { Lingerie } from 'src/app/shared/models/lingerie';
-// import '~bootstrap/dist/css/bootstrap.css';
-// import 'bootstrap-icons';
 
 @Component({
   selector: 'app-home',
