0425a58b5a3e5d4aed0f909c335e0e4cbfeb8ef3
athenacats <124017569+athenacats@users.noreply.github.com>
2023-06-15 15:37:22 +0300

    Add html and ts for search

--- frontend/src/app/components/partials/search/search.component.ts
+++ frontend/src/app/components/partials/search/search.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
 })
 export class SearchComponent {
-
+  searchTerm = '';
+  constructor(activatedRoute: ActivatedRoute, private router: Router) {
+    activatedRoute.params.subscribe((params) => {
+      if (params['searchTerm']) this.searchTerm = params['searchTerm'];
+    });
+  }
+  search(term: string): void {
+    if (term) this.router.navigateByUrl('/search/' + term);
+  }
 }
