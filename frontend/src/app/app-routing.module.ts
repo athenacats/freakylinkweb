f1b07f91a32d044ad54e79658086767f942705d3
athenacats <124017569+athenacats@users.noreply.github.com>
2023-06-16 11:55:18 +0300

    Add ts

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
+import { LingeriePageComponent } from './components/pages/lingerie-page/lingerie-page.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'search/:searchTerm', component: HomeComponent },
   { path: 'tag/:tag', component: HomeComponent },
+  { path: 'lingerie/:id', component: LingeriePageComponent },
 ];
 
 @NgModule({
--- frontend/src/app/components/pages/lingerie-page/lingerie-page.component.ts
+++ frontend/src/app/components/pages/lingerie-page/lingerie-page.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { LingerieService } from 'src/app/services/lingerie.service';
+import { Lingerie } from 'src/app/shared/models/lingerie';
 
 @Component({
   selector: 'app-lingerie-page',
   templateUrl: './lingerie-page.component.html',
-  styleUrls: ['./lingerie-page.component.css']
+  styleUrls: ['./lingerie-page.component.css'],
 })
 export class LingeriePageComponent {
-
+  lingerie!: Lingerie;
+  constructor(
+    activatedRoute: ActivatedRoute,
+    lingerieService: LingerieService
+  ) {
+    activatedRoute.params.subscribe((params) => {
+      if (params['id'])
+        this.lingerie = lingerieService.getLingerieById(params['id']);
+    });
+  }
 }
