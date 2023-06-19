278333940e54d572fd24ff0537d8f3a11bc068de
athenacats <124017569+athenacats@users.noreply.github.com>
2023-06-19 09:26:07 +0300

    Fix header cart button not working

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
 import { LingeriePageComponent } from './components/pages/lingerie-page/lingerie-page.component';
+import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'search/:searchTerm', component: HomeComponent },
   { path: 'tag/:tag', component: HomeComponent },
   { path: 'lingerie/:id', component: LingeriePageComponent },
+  { path: 'cart-page', component: CartPageComponent },
 ];
 
 @NgModule({
