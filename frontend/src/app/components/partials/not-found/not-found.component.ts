e47811d9e9ce50a0d72440f46078bb3dc58c33fa
athenacats <124017569+athenacats@users.noreply.github.com>
2023-06-19 15:23:52 +0300

    Add ts and html for not found

--- frontend/src/app/components/partials/not-found/not-found.component.ts
+++ frontend/src/app/components/partials/not-found/not-found.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.css']
+  styleUrls: ['./not-found.component.css'],
 })
 export class NotFoundComponent {
-
+  @Input()
+  visible = false;
+  @Input()
+  resetLinkText = 'Nothing Found!';
+  @Input()
+  notFoundMessage = 'Nothing Found!';
+  @Input()
+  resetLinkRoute = '/';
 }
