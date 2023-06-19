4790f9220d71b20b519d3ecf63e54e0a6f8683be
athenacats <124017569+athenacats@users.noreply.github.com>
2023-06-19 09:43:25 +0300

    Add ts for actions doable on cart page

--- frontend/src/app/components/pages/cart-page/cart-page.component.ts
+++ frontend/src/app/components/pages/cart-page/cart-page.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
+import { Cart } from 'src/app/shared/models/Cart';
+import { CartItem } from 'src/app/shared/models/CartItem';
 
 @Component({
   selector: 'app-cart-page',
   templateUrl: './cart-page.component.html',
-  styleUrls: ['./cart-page.component.css']
+  styleUrls: ['./cart-page.component.css'],
 })
 export class CartPageComponent {
+  cart!: Cart;
+  constructor(private cartService: CartService) {
+    this.cartService.getCartObservable().subscribe((cart) => {
+      this.cart = cart;
+    });
+  }
 
+  removeFromCart(cartItem: CartItem) {
+    this.cartService.removeFromCart(cartItem.lingerie.id);
+  }
+
+  changeQuantity(cartItem: CartItem, quantityInString: string) {
+    const newQuantity = parseInt(quantityInString);
+    this.cartService.changeQuantity(cartItem.lingerie.id, newQuantity);
+  }
 }
