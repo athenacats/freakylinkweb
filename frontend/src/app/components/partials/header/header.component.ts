ad03b228e03b9889f14274f50947c62782f24479
athenacats <124017569+athenacats@users.noreply.github.com>
2023-06-19 14:27:04 +0300

    Add ts for cart quantity

--- frontend/src/app/components/partials/header/header.component.ts
+++ frontend/src/app/components/partials/header/header.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { subscribeOn } from 'rxjs';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
-
+  cartQuantity = 0;
+  constructor(cartService: CartService) {
+    cartService.getCartObservable().subscribe((newCart) => {
+      this.cartQuantity = newCart.totalCount;
+    });
+  }
 }
