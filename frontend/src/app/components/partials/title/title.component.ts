4a1c3f1238fbaad472dcaad168a7a064d408377c
athenacats <124017569+athenacats@users.noreply.github.com>
2023-06-19 09:52:49 +0300

    Set TS for title page

--- frontend/src/app/components/partials/title/title.component.ts
+++ frontend/src/app/components/partials/title/title.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, INJECTOR, Input } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
-  styleUrls: ['./title.component.css']
+  styleUrls: ['./title.component.css'],
 })
 export class TitleComponent {
+  @Input()
+  title!: string;
 
+  @Input()
+  margin? = '1rem 0 1rem 0.2rem';
+
+  @Input()
+  fontSize? = '1.7rem';
 }
