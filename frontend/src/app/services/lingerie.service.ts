04bb5872cb309db3bf4f3b6039a499be47466360
athenacats <124017569+athenacats@users.noreply.github.com>
2023-06-20 09:41:22 +0300

    Fix get all by connecting to urls

--- frontend/src/app/services/lingerie.service.ts
+++ frontend/src/app/services/lingerie.service.ts
@@ -2,37 +2,41 @@ import { Injectable } from '@angular/core';
 import { Lingerie } from '../shared/models/lingerie';
 import { sampleLingerie, sampleTags } from 'src/data';
 import { Tag } from '../shared/models/tag';
+import { HttpClient } from '@angular/common/http';
+import {
+  LINGERIES_BY_ID_URL,
+  LINGERIES_BY_SEARCH_URL,
+  LINGERIES_TAGS_URL,
+  LINGERIES_URL,
+} from '../shared/constants/urls';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LingerieService {
   // eslint-disable-next-line @typescript-eslint/no-empty-function
-  constructor() {}
-  getAll(): Lingerie[] {
-    return sampleLingerie;
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<Lingerie[]> {
+    return this.http.get<Lingerie[]>(LINGERIES_URL);
   }
 
   getAllLingerieBySearchTerm(searchTerm: string) {
-    return this.getAll().filter((Lingerie) =>
-      Lingerie.name.toLowerCase().includes(searchTerm.toLowerCase())
-    );
+    return this.http.get<Lingerie[]>(LINGERIES_BY_SEARCH_URL + searchTerm);
   }
 
-  getAllTags(): Tag[] {
-    return sampleTags;
+  getAllTags(): Observable<Tag[]> {
+    return this.http.get<Tag[]>(LINGERIES_TAGS_URL);
   }
 
-  getAllLingerieByTag(tag: string): Lingerie[] {
+  getAllLingerieByTag(tag: string): Observable<Lingerie[]> {
     return tag == 'All'
       ? this.getAll()
-      : this.getAll().filter((lingerie) => lingerie.tags?.includes(tag));
+      : this.http.get<Lingerie[]>(LINGERIES_BY_SEARCH_URL + tag);
   }
 
-  getLingerieById(lingerieId: string): Lingerie {
-    return (
-      this.getAll().find((lingerie) => lingerie.id == lingerieId) ??
-      new Lingerie()
-    );
+  getLingerieById(lingerieId: string): Observable<Lingerie> {
+    return this.http.get<Lingerie>(LINGERIES_BY_ID_URL + lingerieId);
   }
 }
