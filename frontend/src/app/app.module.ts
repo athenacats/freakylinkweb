56f8a34b8eaf0a82b8490e26b877fa2e23a075b2
athenacats <124017569+athenacats@users.noreply.github.com>
2023-06-20 09:31:10 +0300

    Add http client module to app module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/partials/header/header.component';
@@ -25,7 +25,7 @@ import { NotFoundComponent } from './components/partials/not-found/not-found.com
     TitleComponent,
     NotFoundComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, NgbModule],
+  imports: [BrowserModule, AppRoutingModule, NgbModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
