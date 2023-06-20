8fe0d303366907cf220ff91da31e523e7c40e818
athenacats <124017569+athenacats@users.noreply.github.com>
2023-06-20 09:21:34 +0300

    Add api for id and tag

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -26,6 +26,21 @@ app.get("/api/lingeries/tags", (req, res) => {
   res.send(sampleTags);
 });
 
+app.get("/api/lingeries/tag/:tagName", (req, res) => {
+  const tagName = req.params.tagName;
+  const lingeries = sampleLingerie.filter((lingerie) =>
+    lingerie.tags?.includes(tagName)
+  );
+  res.send(lingeries);
+});
+
+app.get("/api/lingeries/:lingerieId", (req, res) => {
+  const lingerieId = req.params.lingerieId;
+  const lingeries = sampleLingerie.find(
+    (lingerie) => lingerie.id == lingerieId
+  );
+  res.send(lingeries);
+});
 const port = 5000;
 app.listen(port, () => {
   console.log("Server is running on http://localhost:" + port);
