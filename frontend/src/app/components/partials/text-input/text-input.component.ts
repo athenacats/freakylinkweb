35d3073d66f10649f4f2e9a8245ee2060c35eaca
athenacats <124017569+athenacats@users.noreply.github.com>
2023-06-20 16:27:54 +0300

    Define its typescript

--- frontend/src/app/components/partials/text-input/text-input.component.ts
+++ frontend/src/app/components/partials/text-input/text-input.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-text-input',
   templateUrl: './text-input.component.html',
-  styleUrls: ['./text-input.component.css']
+  styleUrls: ['./text-input.component.css'],
 })
 export class TextInputComponent {
-
+  @Input()
+  control!: AbstractControl;
+  @Input()
+  showErrorsWhen = true;
+  @Input()
+  label!: string;
+  @Input()
+  type: 'text' | 'password' | 'email' = 'text';
 }
