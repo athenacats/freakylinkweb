071c4c265ef470befa2de6034e092565f4e7c43f
athenacats <124017569+athenacats@users.noreply.github.com>
2023-06-20 16:14:25 +0300

    Add app validation bts

--- frontend/src/app/components/partials/input-validation/input-validation.component.ts
+++ frontend/src/app/components/partials/input-validation/input-validation.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+/* eslint-disable @angular-eslint/component-selector */
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
+import { AbstractControl } from '@angular/forms';
 
+const VALIDATORS_MESSAGES: any = {
+  required: 'Should not be empty',
+  email: 'Email is not valid',
+};
 @Component({
-  selector: 'app-input-validation',
+  selector: 'input-validation',
   templateUrl: './input-validation.component.html',
-  styleUrls: ['./input-validation.component.css']
+  styleUrls: ['./input-validation.component.css'],
 })
-export class InputValidationComponent {
+export class InputValidationComponent implements OnChanges, OnInit {
+  ngOnInit(): void {
+    this.control.statusChanges.subscribe(() => {
+      this.checkValidation();
+    });
+    this.control.valueChanges.subscribe(() => {
+      this.checkValidation();
+    });
+  }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    this.checkValidation();
+  }
+  @Input()
+  control!: AbstractControl;
+  @Input()
+  showErrorsWhen = true;
+  errorMessages: string[] = [];
+
+  checkValidation() {
+    const errors = this.control.errors;
+    if (!errors) {
+      this.errorMessages = [];
+      return;
+    }
+
+    const errorKeys = Object.keys(errors);
+    this.errorMessages = errorKeys.map((key) => VALIDATORS_MESSAGES[key]);
+  }
 }
