720dc0fabf648fa67f61affbf5606e185a74c943
athenacats <124017569+athenacats@users.noreply.github.com>
2023-06-20 15:08:23 +0300

    Implement finding of user in header ts

--- frontend/src/app/components/partials/header/header.component.ts
+++ frontend/src/app/components/partials/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { CartService } from 'src/app/services/cart.service';
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/shared/models/user';
 
 @Component({
   selector: 'app-header',
@@ -8,9 +10,13 @@ import { CartService } from 'src/app/services/cart.service';
 })
 export class HeaderComponent {
   cartQuantity = 0;
-  constructor(cartService: CartService) {
+  user!: User;
+  constructor(cartService: CartService, userService: UserService) {
     cartService.getCartObservable().subscribe((newCart) => {
       this.cartQuantity = newCart.totalCount;
+    });
+    userService.userObservable.subscribe((newUser) => {
+      this.user = newUser;
     });
   }
 }
