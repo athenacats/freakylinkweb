2ee6224311cc3da52f2dff6ebc39c6b0c44d29a5
athenacats <124017569+athenacats@users.noreply.github.com>
2023-06-20 13:41:47 +0300

    Create api for login

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
-import { sampleLingerie, sampleTags } from "./data";
+import { sampleLingerie, sampleTags, sampleUsers } from "./data";
+import jwt from "jsonwebtoken";
 
 const app = express();
 app.use(express.json());
@@ -44,8 +45,29 @@ app.get("/api/lingeries/:lingerieId", (req, res) => {
 });
 
 app.post("/api/users/login", (req, res) => {
-  const body = req.body;
+  const { email, password } = req.body;
+  const user = sampleUsers.find(
+    (user) => user.email === email && user.password === password
+  );
+  if (user) {
+    res.send(generateTokenResponse(user));
+  } else {
+    res.status(400).send("Username or password is not valid!");
+  }
 });
+
+const generateTokenResponse = (user: any) => {
+  const token = jwt.sign(
+    {
+      email: user.email,
+      isAdmin: user.isAdmin,
+    },
+    "xyz",
+    { expiresIn: "30d" }
+  );
+  user.token = token;
+  return user;
+};
 
 const port = 5000;
 app.listen(port, () => {
