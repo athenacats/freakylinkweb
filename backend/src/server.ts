1587e2aa50395c05956a9e9cfdbed2549555717d
athenacats <124017569+athenacats@users.noreply.github.com>
2023-06-20 10:32:55 +0300

    Remove / from origin under cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,10 +3,11 @@ import cors from "cors";
 import { sampleLingerie, sampleTags } from "./data";
 
 const app = express();
+
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:4200/"],
+    origin: ["http://localhost:4200"],
   })
 );
 
