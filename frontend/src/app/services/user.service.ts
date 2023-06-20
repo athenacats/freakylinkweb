7210c5921b53741727f472d831c1dcd1093fce50
athenacats <124017569+athenacats@users.noreply.github.com>
2023-06-20 14:15:24 +0300

    Implement code for login

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
 import { User } from '../shared/models/user';
+import { IUserLogin } from '../shared/interfaces/IUserLogin';
+import { HttpClient } from '@angular/common/http';
+import { USER_LOGIN_URL } from '../shared/constants/urls';
 
 @Injectable({
   providedIn: 'root',
@@ -9,9 +12,16 @@ export class UserService {
   private userSubject = new BehaviorSubject<User>(new User());
   public userObservable: Observable<User>;
 
-  constructor() {
+  constructor(private http: HttpClient) {
     this.userObservable = this.userSubject.asObservable();
   }
 
-  login(userLogin: IUserLogin);
+  login(userLogin: IUserLogin): Observable<User> {
+    return this.http.post<User>(USER_LOGIN_URL, userLogin).pipe(
+      tap({
+        next: (user) => {},
+        error: (errorResponse) => {},
+      })
+    );
+  }
 }
--- frontend/src/app/shared/constants/urls.ts
+++ frontend/src/app/shared/constants/urls.ts
@@ -5,3 +5,4 @@ export const LINGERIES_TAGS_URL = LINGERIES_URL + '/tags';
 export const LINGERIES_BY_SEARCH_URL = LINGERIES_URL + '/search/';
 export const LINGERIES_BY_TAG_URL = LINGERIES_URL + '/tag/';
 export const LINGERIES_BY_ID_URL = LINGERIES_URL + '/';
+export const USER_LOGIN_URL = BASE_URL + '/api/users/login';
