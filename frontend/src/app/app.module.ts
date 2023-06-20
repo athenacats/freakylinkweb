6cf493fdc8c47b38098fc1c5ea8e96ccbe693a26
athenacats <124017569+athenacats@users.noreply.github.com>
2023-06-20 16:25:02 +0300

    Generate text input component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -18,6 +18,7 @@ import { ToastrModule } from 'ngx-toastr';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { InputContainerComponent } from './components/partials/input-container/input-container.component';
 import { InputValidationComponent } from './components/partials/input-validation/input-validation.component';
+import { TextInputComponent } from './components/partials/text-input/text-input.component';
 
 @NgModule({
   declarations: [
@@ -33,6 +34,7 @@ import { InputValidationComponent } from './components/partials/input-validation
     LoginPageComponent,
     InputContainerComponent,
     InputValidationComponent,
+    TextInputComponent,
   ],
   imports: [
     BrowserModule,
