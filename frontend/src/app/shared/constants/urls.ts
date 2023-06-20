b9dd9474c9e1017536e470a32c64cca4ac02828b
athenacats <124017569+athenacats@users.noreply.github.com>
2023-06-20 09:28:13 +0300

    Create base urls

---
+++ frontend/src/app/shared/constants/urls.ts
@@ -0,0 +1,7 @@
+const BASE_URL = 'http://localhost:5000';
+
+export const LINGERIES_URL = BASE_URL + '/api/lingeries';
+export const LINGERIES_TAGS_URL = LINGERIES_URL + '/tags';
+export const LINGERIES_BY_SEARCH_URL = LINGERIES_URL + '/search/';
+export const LINGERIES_BY_TAG_URL = LINGERIES_URL + '/tag/';
+export const LINGERIES_BY_ID_URL = LINGERIES_URL + '/';
