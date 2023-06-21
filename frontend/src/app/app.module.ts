3461e686e4ea2f173bc0825038c59b706676140d
athenacats <124017569+athenacats@users.noreply.github.com>
2023-06-21 09:36:38 +0300

    Create component for default button

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -19,6 +19,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { InputContainerComponent } from './components/partials/input-container/input-container.component';
 import { InputValidationComponent } from './components/partials/input-validation/input-validation.component';
 import { TextInputComponent } from './components/partials/text-input/text-input.component';
+import { DefaultButtonComponent } from './components/partials/default-button/default-button.component';
 
 @NgModule({
   declarations: [
@@ -35,6 +36,7 @@ import { TextInputComponent } from './components/partials/text-input/text-input.
     InputContainerComponent,
     InputValidationComponent,
     TextInputComponent,
+    DefaultButtonComponent,
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/components/partials/default-button/default-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-default-button',
+  templateUrl: './default-button.component.html',
+  styleUrls: ['./default-button.component.css']
+})
+export class DefaultButtonComponent {
+
+}
