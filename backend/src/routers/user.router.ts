73d4db9d96dd49596e48e54369bdcb0548d60fac
athenacats <124017569+athenacats@users.noreply.github.com>
2023-06-21 12:46:40 +0300

    Seed the users in the database

--- backend/src/routers/user.router.ts
+++ backend/src/routers/user.router.ts
@@ -1,8 +1,23 @@
 import { Router } from "express";
 import { sampleUsers } from "../data";
 import jwt from "jsonwebtoken";
+import asyncHandler from "express-async-handler";
+import { UserModel } from "../models/user.model";
 
 const router = Router();
+
+router.get(
+  "/seed",
+  asyncHandler(async (req, res) => {
+    const userCount = await UserModel.countDocuments();
+    if (userCount > 0) {
+      res.send("Seed is already done!");
+      return;
+    }
+    await UserModel.create(sampleUsers);
+    res.send("Seed is done!");
+  })
+);
 
 router.post("/login", (req, res) => {
   const { email, password } = req.body;
