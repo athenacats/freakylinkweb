e47125a63f5ecea7bc979feabada3466659ea571
athenacats <124017569+athenacats@users.noreply.github.com>
2023-06-21 22:07:29 +0300

    Finish on code for registering new user

--- backend/src/routers/user.router.ts
+++ backend/src/routers/user.router.ts
@@ -2,8 +2,9 @@ import { Router } from "express";
 import { sampleUsers } from "../data";
 import jwt from "jsonwebtoken";
 import asyncHandler from "express-async-handler";
-import { UserModel } from "../models/user.model";
+import { User, UserModel } from "../models/user.model";
 import bcrypt from "bcryptjs";
+import { HTTP_BAD_REQUEST } from "../constants/http_status";
 
 const router = Router();
 
@@ -29,7 +30,7 @@ router.post(
     if (user) {
       res.send(generateTokenResponse(user));
     } else {
-      res.status(400).send("Username or password is not valid!");
+      res.status(HTTP_BAD_REQUEST).send("Username or password is not valid!");
     }
   })
 );
@@ -40,8 +41,21 @@ router.post(
     const { name, email, password, address } = req.body;
     const user = await UserModel.findOne({ email });
     if (user) {
-      res.status;
+      res.status(HTTP_BAD_REQUEST).send("User already exists. Please login!");
+      return;
     }
+    const encryptedPassword = await bcrypt.hash(password, 10);
+
+    const newUser: User = {
+      id: "",
+      name,
+      email: email.toLowerCase(),
+      password: encryptedPassword,
+      address,
+      isAdmin: false,
+    };
+    const dbUser = await UserModel.create(newUser);
+    res.send(generateTokenResponse(dbUser));
   })
 );
 
