7bb64509ab5db95c0ed4e8fb7e17646727d6b98f
athenacats <124017569+athenacats@users.noreply.github.com>
2023-06-21 21:30:01 +0300

    Implement login. Bug: not working

--- backend/src/routers/user.router.ts
+++ backend/src/routers/user.router.ts
@@ -19,17 +19,19 @@ router.get(
   })
 );
 
-router.post("/login", (req, res) => {
-  const { email, password } = req.body;
-  const user = sampleUsers.find(
-    (user) => user.email === email && user.password === password
-  );
-  if (user) {
-    res.send(generateTokenResponse(user));
-  } else {
-    res.status(400).send("Username or password is not valid!");
-  }
-});
+router.post(
+  "/login",
+  asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    const user = await UserModel.findOne({ email, password });
+    if (user) {
+      res.send(generateTokenResponse(user));
+    } else {
+      const BAD_REQUEST = 400;
+      res.status(BAD_REQUEST).send("Username or password is not valid!");
+    }
+  })
+);
 
 const generateTokenResponse = (user: any) => {
   const token = jwt.sign(
