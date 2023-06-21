73a38d175579a0c1d1e9f0e87bcc160d1a11d635
athenacats <124017569+athenacats@users.noreply.github.com>
2023-06-21 09:44:37 +0300

    Set up ts for default button

--- frontend/src/app/components/partials/default-button/default-button.component.ts
+++ frontend/src/app/components/partials/default-button/default-button.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-default-button',
   templateUrl: './default-button.component.html',
-  styleUrls: ['./default-button.component.css']
+  styleUrls: ['./default-button.component.css'],
 })
 export class DefaultButtonComponent {
-
+  @Input()
+  type: 'submit' | 'button' = 'submit';
+  @Input()
+  text = 'Submit';
+  @Input()
+  bgColor = 'var(--brighterColor)';
+  @Input()
+  color = 'var(--shadowSaturated)';
+  @Input()
+  fontSizeRem = 1.3;
+  @Input()
+  widthRem = 12;
+  @Output()
+  clickEvent = new EventEmitter();
 }
