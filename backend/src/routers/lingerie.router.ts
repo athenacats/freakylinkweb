caf0edbfad6aaa263ea3d81de96c4a694979b5a5
athenacats <124017569+athenacats@users.noreply.github.com>
2023-06-21 10:07:10 +0300

    Remove redundant /api/lingerie fromrouter

--- backend/src/routers/lingerie.router.ts
+++ backend/src/routers/lingerie.router.ts
@@ -3,11 +3,11 @@ import { sampleLingerie, sampleTags } from "../data";
 
 const router = Router();
 
-router.get("/api/lingeries", (req, res) => {
+router.get("/", (req, res) => {
   res.send(sampleLingerie);
 });
 
-router.get("/api/lingeries/search/:searchTerm", (req, res) => {
+router.get("/search/:searchTerm", (req, res) => {
   const searchTerm = req.params.searchTerm;
   const lingeries = sampleLingerie.filter((Lingerie) =>
     Lingerie.name.toLowerCase().includes(searchTerm.toLowerCase())
@@ -15,11 +15,11 @@ router.get("/api/lingeries/search/:searchTerm", (req, res) => {
   res.send(lingeries);
 });
 
-router.get("/api/lingeries/tags", (req, res) => {
+router.get("/tags", (req, res) => {
   res.send(sampleTags);
 });
 
-router.get("/api/lingeries/tag/:tagName", (req, res) => {
+router.get("/tag/:tagName", (req, res) => {
   const tagName = req.params.tagName;
   const lingeries = sampleLingerie.filter((lingerie) =>
     lingerie.tags?.includes(tagName)
@@ -27,7 +27,7 @@ router.get("/api/lingeries/tag/:tagName", (req, res) => {
   res.send(lingeries);
 });
 
-router.get("/api/lingeries/:lingerieId", (req, res) => {
+router.get("/:lingerieId", (req, res) => {
   const lingerieId = req.params.lingerieId;
   const lingeries = sampleLingerie.find(
     (lingerie) => lingerie.id == lingerieId
