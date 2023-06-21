060c487471877f13fe09cd2b9dc2c92b4ee91979
athenacats <124017569+athenacats@users.noreply.github.com>
2023-06-21 12:25:05 +0300

    Create lingerie model

---
+++ backend/src/models/lingerie.model.ts
@@ -0,0 +1,36 @@
+import { Schema, model } from "mongoose";
+
+export interface Lingerie {
+  id: string;
+  name: string;
+  price: number;
+  tags: string[];
+  favorite: boolean;
+  stars: number;
+  imageUrl: string;
+  description: string;
+  color: string;
+}
+
+export const LingerieSchema = new Schema<Lingerie>(
+  {
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    tags: { type: [String] },
+    favorite: { type: Boolean, default: false },
+    stars: { type: Number, required: true },
+    imageUrl: { type: String, required: true },
+    color: { type: String, required: true },
+  },
+  {
+    toJSON: {
+      virtuals: true, //this makes the id to be converted to mongoose _id
+    },
+    toObject: {
+      virtuals: true,
+    },
+    timestamps: true,
+  }
+);
+
+export const LIngerieModel = model<Lingerie>("lingerie", LingerieSchema);
