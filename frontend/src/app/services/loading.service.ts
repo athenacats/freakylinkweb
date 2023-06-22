6b03e8124bba843e00459b5a80322e1b044cbed7
athenacats <124017569+athenacats@users.noreply.github.com>
2023-06-22 11:04:21 +0300

    Add loading service hide and show

--- frontend/src/app/services/loading.service.ts
+++ frontend/src/app/services/loading.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LoadingService {
+  private isLoadingSubject = new BehaviorSubject<boolean>(false);
 
-  constructor() { }
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  constructor() {}
+
+  showLoading() {
+    this.isLoadingSubject.next(true);
+  }
+
+  hideLoading() {
+    this.isLoadingSubject.next(false);
+  }
+
+  get isLoading() {
+    return this.isLoadingSubject.asObservable();
+  }
 }
