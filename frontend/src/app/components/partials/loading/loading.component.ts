67318bf63215c9b3cbb77510bf5eb3b8e87ce248
athenacats <124017569+athenacats@users.noreply.github.com>
2023-06-22 11:07:02 +0300

    Create ts for loading

--- frontend/src/app/components/partials/loading/loading.component.ts
+++ frontend/src/app/components/partials/loading/loading.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { LoadingService } from 'src/app/services/loading.service';
 
 @Component({
   selector: 'app-loading',
   templateUrl: './loading.component.html',
-  styleUrls: ['./loading.component.css']
+  styleUrls: ['./loading.component.css'],
 })
 export class LoadingComponent {
+  isLoading!: boolean;
 
+  constructor(loadingService: LoadingService) {
+    loadingService.isLoading.subscribe((isLoading) => {
+      this.isLoading = isLoading;
+    });
+  }
 }
