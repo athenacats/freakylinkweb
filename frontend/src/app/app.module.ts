fb51992eb1280077a07f3b4eec08854d9ec7645e
athenacats <124017569+athenacats@users.noreply.github.com>
2023-06-22 17:10:59 +0300

    Create map component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -25,6 +25,7 @@ import { LoadingComponent } from './components/partials/loading/loading.componen
 import { LoadingInterceptor } from './shared/interceptors/loading.interceptor';
 import { CheckoutPageComponent } from './components/pages/checkout-page/checkout-page.component';
 import { OrderItemsListComponent } from './components/partials/order-items-list/order-items-list.component';
+import { MapComponent } from './components/partials/map/map.component';
 
 @NgModule({
   declarations: [
@@ -46,6 +47,7 @@ import { OrderItemsListComponent } from './components/partials/order-items-list/
     LoadingComponent,
     CheckoutPageComponent,
     OrderItemsListComponent,
+    MapComponent,
   ],
   imports: [
     BrowserModule,
