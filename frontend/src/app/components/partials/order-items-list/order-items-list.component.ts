b3ad331b9925e9c171447f78cc985f4ced351e54
athenacats <124017569+athenacats@users.noreply.github.com>
2023-06-22 14:19:25 +0300

    Set up html for order items list

--- frontend/src/app/components/partials/order-items-list/order-items-list.component.ts
+++ frontend/src/app/components/partials/order-items-list/order-items-list.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Order } from 'src/app/shared/models/order';
 
 @Component({
   selector: 'app-order-items-list',
   templateUrl: './order-items-list.component.html',
-  styleUrls: ['./order-items-list.component.css']
+  styleUrls: ['./order-items-list.component.css'],
 })
 export class OrderItemsListComponent {
-
+  @Input()
+  order!: Order;
 }
