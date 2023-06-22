a50eca648b3ea9634580b640295870c90932a42f
athenacats <124017569+athenacats@users.noreply.github.com>
2023-06-22 13:56:37 +0300

    Set up ts checkout page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { LingeriePageComponent } from './components/pages/lingerie-page/lingerie
 import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
 import { LoginPageComponent } from './components/pages/login-page/login-page.component';
 import { RegisterPageComponent } from './components/pages/register-page/register-page.component';
+import { CheckoutPageComponent } from './components/pages/checkout-page/checkout-page.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -14,6 +15,7 @@ const routes: Routes = [
   { path: 'cart-page', component: CartPageComponent },
   { path: 'login', component: LoginPageComponent },
   { path: 'register', component: RegisterPageComponent },
+  { path: 'checkout', component: CheckoutPageComponent },
 ];
 
 @NgModule({
