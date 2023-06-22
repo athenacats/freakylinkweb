e850b0ade31df6174345f640eb2b9f1ceb99a979
athenacats <124017569+athenacats@users.noreply.github.com>
2023-06-22 13:47:24 +0300

    Define getCart to be used in checkout

--- frontend/src/app/services/cart.service.ts
+++ frontend/src/app/services/cart.service.ts
@@ -48,6 +48,10 @@ export class CartService {
     return this.cartSubject.asObservable();
   }
 
+  getCart(): Cart {
+    return this.cartSubject.value;
+  }
+
   private setCartToLocalStorage(): void {
     this.cart.totalPrice = this.cart.items.reduce(
       (prevSum, currentItem) => prevSum + currentItem.price,
