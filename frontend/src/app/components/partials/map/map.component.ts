92e2f4fd709055b02a8c3a0f5cfce55f00dc5c2e
athenacats <124017569+athenacats@users.noreply.github.com>
2023-06-22 17:21:09 +0300

    Add map ts to map.component

--- frontend/src/app/components/partials/map/map.component.ts
+++ frontend/src/app/components/partials/map/map.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+/* eslint-disable @angular-eslint/component-selector */
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { LatLngTuple, Map, map, tileLayer } from 'leaflet';
 
 @Component({
-  selector: 'app-map',
+  selector: 'map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.css']
+  styleUrls: ['./map.component.css'],
 })
 export class MapComponent {
+  private readonly DEFAULT_LATLNG: LatLngTuple = [1.2921, 36.8219];
+  @ViewChild('map', { static: true })
+  mapElement!: ElementRef;
 
+  map!: Map;
+
+  initializeMap() {
+    if (this.map) return;
+    this.map = map(this.mapElement.nativeElement, {
+      attributionControl: false,
+    }).setView(this.DEFAULT_LATLNG, 1);
+
+    tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(this.map);
+  }
 }
