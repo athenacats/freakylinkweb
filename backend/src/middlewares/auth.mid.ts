dbd42f5f7d51172e9e746eb719c49b408e91e384
athenacats <124017569+athenacats@users.noreply.github.com>
2023-06-23 19:55:42 +0300

    Create authentication middleware

---
+++ backend/src/middlewares/auth.mid.ts
@@ -0,0 +1,15 @@
+import { verify } from "jsonwebtoken";
+import { HTTP_UNAUTHORIZED_REQUEST } from "../constants/http_status";
+
+export default (req: any, res: any, next: any) => {
+  const token = req.headers.access_token as string;
+  if (!token) return res.status(HTTP_UNAUTHORIZED_REQUEST);
+
+  try {
+    const decodedUser = verify(token, process.env.JWT_SECRET!);
+    req.user = decodedUser;
+  } catch (error) {
+    res.status(HTTP_UNAUTHORIZED_REQUEST).send();
+  }
+  return next();
+};
