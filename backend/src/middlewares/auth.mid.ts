fc32d8e6d48a5075116931baf59db6c7df58d82f
athenacats <124017569+athenacats@users.noreply.github.com>
2023-06-23 20:32:14 +0300

    Bug: proceed to payment not working

--- backend/src/middlewares/auth.mid.ts
+++ backend/src/middlewares/auth.mid.ts
@@ -3,7 +3,7 @@ import { HTTP_UNAUTHORIZED_REQUEST } from "../constants/http_status";
 
 export default (req: any, res: any, next: any) => {
   const token = req.headers.access_token as string;
-  if (!token) return res.status(HTTP_UNAUTHORIZED_REQUEST);
+  if (!token) return res.status(HTTP_UNAUTHORIZED_REQUEST).send();
 
   try {
     const decodedUser = verify(token, process.env.JWT_SECRET!);
