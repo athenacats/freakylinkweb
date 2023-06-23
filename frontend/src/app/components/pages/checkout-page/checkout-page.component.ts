f32cf4fa8933169e0450a9f3602a0fbac0536a82
athenacats <124017569+athenacats@users.noreply.github.com>
2023-06-23 20:20:55 +0300

    Include order model on checkout page ts

--- frontend/src/app/components/pages/checkout-page/checkout-page.component.ts
+++ frontend/src/app/components/pages/checkout-page/checkout-page.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { CartService } from 'src/app/services/cart.service';
+import { OrderService } from 'src/app/services/order.service';
 import { UserService } from 'src/app/services/user.service';
 import { Order } from 'src/app/shared/models/order';
 
@@ -17,7 +19,9 @@ export class CheckoutPageComponent implements OnInit {
     cartService: CartService,
     private formBuilder: FormBuilder,
     private userService: UserService,
-    private toastrService: ToastrService
+    private toastrService: ToastrService,
+    private orderService: OrderService,
+    private router: Router
   ) {
     const cart = cartService.getCart();
     this.order.items = cart.items;
@@ -40,9 +44,23 @@ export class CheckoutPageComponent implements OnInit {
       this.toastrService.warning('Please fill the inputs', 'Invalid inputs');
       return;
     }
+
+    if (!this.order.addressLatLng) {
+      this.toastrService.warning(
+        'Please select your location on the map',
+        'Location'
+      );
+    }
     this.order.name = this.fc['name'].value;
     this.order.address = this.fc['address'].value;
 
-    console.log(this.order);
+    this.orderService.create(this.order).subscribe({
+      next: () => {
+        this.router.navigateByUrl('/payment');
+      },
+      error: (errorResponse) => {
+        this.toastrService.error(errorResponse.error, 'Cart');
+      },
+    });
   }
 }
