ee7e79ee7c4557331c25d8763d218783d1a9cd5e
athenacats <124017569+athenacats@users.noreply.github.com>
2023-06-23 12:05:57 +0300

    Set the code to make the map responsible

--- frontend/src/app/components/partials/map/map.component.ts
+++ frontend/src/app/components/partials/map/map.component.ts
@@ -1,6 +1,19 @@
 /* eslint-disable @angular-eslint/component-selector */
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { LatLngTuple, Map, map, tileLayer } from 'leaflet';
+import {
+  LatLng,
+  LatLngExpression,
+  LatLngTuple,
+  LeafletMouseEvent,
+  Map,
+  Marker,
+  icon,
+  latLng,
+  map,
+  marker,
+  tileLayer,
+} from 'leaflet';
+import { LocationService } from 'src/app/services/location.service';
 
 @Component({
   selector: 'map',
@@ -8,11 +21,22 @@ import { LatLngTuple, Map, map, tileLayer } from 'leaflet';
   styleUrls: ['./map.component.css'],
 })
 export class MapComponent implements OnInit {
+  private readonly MARKER_ZOOM_LEVEL = 16;
+  private readonly MARKER_ICON = icon({
+    iconUrl:
+      'https://res.cloudinary.com/foodmine/image/upload/v1638842791/map/marker_kbua9q.png',
+    iconSize: [42, 42],
+    iconAnchor: [21, 42],
+  });
+
   private readonly DEFAULT_LATLNG: LatLngTuple = [1.29, 36.82];
   @ViewChild('map', { static: true })
   mapElement!: ElementRef;
 
   map!: Map;
+  currentMarker!: Marker;
+
+  constructor(private locationService: LocationService) {}
 
   ngOnInit(): void {
     this.initializeMap();
@@ -25,6 +49,28 @@ export class MapComponent implements OnInit {
     }).setView(this.DEFAULT_LATLNG, 1);
 
     tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(this.map);
+    this.map.on('click', (e: LeafletMouseEvent) => {
+      this.setMarker(e.latlng);
+    });
   }
-  findMyLocation() {}
+  findMyLocation() {
+    this.locationService.getCurrentLocation().subscribe({
+      next: (latLng) => {
+        this.map.setView(latLng, this.MARKER_ZOOM_LEVEL);
+        this.setMarker(latLng);
+      },
+    });
+  }
+
+  setMarker(latLng: LatLngExpression) {
+    if (this.currentMarker) {
+      this.currentMarker.setLatLng(latLng);
+      return;
+    }
+    this.currentMarker = marker(latLng, {
+      draggable: true,
+      autoPan: true,
+      icon: this.MARKER_ICON,
+    }).addTo(this.map);
+  }
 }
