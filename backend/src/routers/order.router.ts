31bc552bcffb9cb9720ad01e4fee0ea435156a15
athenacats <124017569+athenacats@users.noreply.github.com>
2023-06-23 19:58:45 +0300

    Create order router

---
+++ backend/src/routers/order.router.ts
@@ -0,0 +1,28 @@
+import { Router } from "express";
+import asyncHandler from "express-async-handler";
+import { HTTP_BAD_REQUEST } from "../constants/http_status";
+import { OrderModel } from "../models/order.model";
+import { OrderStatus } from "../constants/order_status";
+import auth from "../middlewares/auth.mid";
+
+const router = Router();
+router.use(auth);
+
+router.post(
+  "/create",
+  asyncHandler(async (req: any, res: any) => {
+    const requestOrder = req.body;
+    if (requestOrder.items.length <= 0) {
+      res.status(HTTP_BAD_REQUEST).send("Cart Is Empty");
+      return;
+    }
+    await OrderModel.deleteOne({
+      user: req.user.id,
+      status: OrderStatus.NEW,
+    });
+
+    const newOrder = new OrderModel({ ...requestOrder, user: req.user.id });
+    await newOrder.save();
+    res.send(newOrder);
+  })
+);
