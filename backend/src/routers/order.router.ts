98d5d4d424c08255149cbf8e0787012170d0452a
athenacats <124017569+athenacats@users.noreply.github.com>
2023-06-26 14:28:47 +0300

    Add the track address to router

--- backend/src/routers/order.router.ts
+++ backend/src/routers/order.router.ts
@@ -56,6 +56,14 @@ router.post(
   })
 );
 
+router.get(
+  "/track/:id",
+  asyncHandler(async (req, res) => {
+    const order = await OrderModel.findById(req.params.id);
+    res.send(order);
+  })
+);
+
 export default router;
 async function getNewOrderForCurrentUser(req: any) {
   return await OrderModel.findOne({
