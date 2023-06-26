1d3b1d28e14a7e525e82017aac5926bf928480d8
athenacats <124017569+athenacats@users.noreply.github.com>
2023-06-26 14:29:54 +0300

    Add the url to url file

--- frontend/src/app/shared/constants/urls.ts
+++ frontend/src/app/shared/constants/urls.ts
@@ -13,3 +13,4 @@ export const ORDERS_CREATE_URL = ORDERS_URL + '/create';
 export const ORDERS_NEW_FOR_CURRENT_USER =
   ORDERS_URL + '/newOrderForCurrentUser';
 export const ORDERS_PAY_URL = ORDERS_URL + '/pay';
+export const ORDERS_TRACK_URL = ORDERS_URL + '/track/';
