95db936e94216aeb95f7383d73846f842498817e
athenacats <124017569+athenacats@users.noreply.github.com>
2023-06-26 10:10:20 +0300

    Add code to payment page ts

--- frontend/src/app/components/pages/payment-page/payment-page.component.ts
+++ frontend/src/app/components/pages/payment-page/payment-page.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { OrderService } from 'src/app/services/order.service';
+import { Order } from 'src/app/shared/models/order';
 
 @Component({
   selector: 'app-payment-page',
   templateUrl: './payment-page.component.html',
-  styleUrls: ['./payment-page.component.css']
+  styleUrls: ['./payment-page.component.css'],
 })
 export class PaymentPageComponent {
+  order: Order = new Order();
 
+  constructor(orderService: OrderService, router: Router) {
+    orderService.getNewOrderForCurrentUser().subscribe({
+      next: (order) => {
+        this.order = order;
+      },
+      error: () => {
+        router.navigateByUrl('/checkout');
+      },
+    });
+  }
 }
