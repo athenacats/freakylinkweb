da4af094d00b09bd8082cb873b0268661e818bfa
athenacats <124017569+athenacats@users.noreply.github.com>
2023-06-26 12:15:37 +0300

    Add paypal route to order router

--- backend/src/routers/order.router.ts
+++ backend/src/routers/order.router.ts
@@ -31,13 +31,35 @@ router.post(
 router.get(
   "/newOrderForCurrentUser",
   asyncHandler(async (req: any, res) => {
-    const order = await OrderModel.findOne({
-      user: req.user.id,
-      status: OrderStatus.NEW,
-    });
+    const order = await getNewOrderForCurrentUser(req);
     if (order) res.send(order);
     else res.status(HTTP_BAD_REQUEST).send();
   })
 );
 
+router.post(
+  "/pay",
+  asyncHandler(async (req: any, res) => {
+    const { paymentId } = req.body;
+    const order = await getNewOrderForCurrentUser(req);
+
+    if (order!) {
+      res.status(HTTP_BAD_REQUEST).send("Order Not Found!");
+      return;
+    }
+
+    order.paymentId = paymentId;
+    order.status = OrderStatus.PAID;
+    await order.save();
+
+    res.send(order._id);
+  })
+);
+
 export default router;
+async function getNewOrderForCurrentUser(req: any) {
+  return await OrderModel.findOne({
+    user: req.user.id,
+    status: OrderStatus.NEW,
+  });
+}
--- frontend/src/app/components/partials/paypal-buttons/paypal-buttons.component.ts
+++ frontend/src/app/components/partials/paypal-buttons/paypal-buttons.component.ts
@@ -1,13 +1,61 @@
 /* eslint-disable @angular-eslint/component-selector */
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { OrderService } from 'src/app/services/order.service';
 import { Order } from 'src/app/shared/models/order';
+
+//window.paypal
+declare let paypal: any;
 
 @Component({
   selector: 'paypal-buttons',
   templateUrl: './paypal-buttons.component.html',
   styleUrls: ['./paypal-buttons.component.css'],
 })
-export class PaypalButtonsComponent {
+export class PaypalButtonsComponent implements OnInit {
   @Input()
   order!: Order;
+  constructor(private orderService: OrderService) {}
+
+  ngOnInit(): void {
+    const self = this;
+    paypal
+      .Buttons({
+        createOrder: (data: any, actions: any) => {
+          return actions.order.create({
+            purchase_units: [
+              {
+                amount: {
+                  currency_code: 'KES',
+                  value: self.order.totalPrice,
+                },
+              },
+            ],
+          });
+        },
+
+        onApprove: async (data: any, actions: any) => {
+          const payment = await actions.order.capture();
+          this.order.paymentId = payment.id;
+          self.orderService.pay(this.order).subscribe({
+            next: (orderId) => {
+              this.cartService.clearCart();
+              this.router.navigateByUrl('/track/' + orderId);
+              this.toastrService.success(
+                'Payment Saved Successfully',
+                'Success'
+              );
+            },
+            error: (error) => {
+              this.toastrService.error('Payment Save Failed', 'Error');
+            },
+          });
+        },
+
+        onError: (err: any) => {
+          this.toastrService.error('Payment Failed', 'Error');
+          console.log(err);
+        },
+      })
+      .render(this.paypalElement.nativeElement);
+  }
 }
