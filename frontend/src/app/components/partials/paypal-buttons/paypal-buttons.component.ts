2abb43735b91d0ee33d54c5c51ca5ddfc9d18b20
athenacats <124017569+athenacats@users.noreply.github.com>
2023-06-26 11:51:44 +0300

    Add paypal component to payment page

--- frontend/src/app/components/partials/paypal-buttons/paypal-buttons.component.ts
+++ frontend/src/app/components/partials/paypal-buttons/paypal-buttons.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+/* eslint-disable @angular-eslint/component-selector */
+import { Component, Input } from '@angular/core';
+import { Order } from 'src/app/shared/models/order';
 
 @Component({
-  selector: 'app-paypal-buttons',
+  selector: 'paypal-buttons',
   templateUrl: './paypal-buttons.component.html',
-  styleUrls: ['./paypal-buttons.component.css']
+  styleUrls: ['./paypal-buttons.component.css'],
 })
 export class PaypalButtonsComponent {
-
+  @Input()
+  order!: Order;
 }
