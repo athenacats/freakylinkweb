1ef87857a029f73293720a916869a30c3da3e514
athenacats <124017569+athenacats@users.noreply.github.com>
2023-06-26 14:36:32 +0300

    Create ts to find order id for tracking

--- frontend/src/app/components/pages/order-track-page/order-track-page.component.ts
+++ frontend/src/app/components/pages/order-track-page/order-track-page.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { OrderService } from 'src/app/services/order.service';
+import { Order } from 'src/app/shared/models/order';
 
 @Component({
   selector: 'app-order-track-page',
   templateUrl: './order-track-page.component.html',
-  styleUrls: ['./order-track-page.component.css']
+  styleUrls: ['./order-track-page.component.css'],
 })
 export class OrderTrackPageComponent {
+  order!: Order;
 
+  constructor(activatedRoute: ActivatedRoute, orderService: OrderService) {
+    const params = activatedRoute.snapshot.params;
+    if (!params['orderId']) return;
+    orderService.trackOrderById(params['orderId']).subscribe((order) => {
+      this.order = order;
+    });
+  }
 }
