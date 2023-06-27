6084ff42a109a14ed7a06424e2e9c33db903f506
athenacats <124017569+athenacats@users.noreply.github.com>
2023-06-27 14:22:03 +0300

    Write html & ts to display user details on profile

--- frontend/src/app/components/pages/profile/profile.component.ts
+++ frontend/src/app/components/pages/profile/profile.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/shared/models/user';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  currentUser!: User;
 
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.currentUser = this.userService.currentUser;
+  }
 }
