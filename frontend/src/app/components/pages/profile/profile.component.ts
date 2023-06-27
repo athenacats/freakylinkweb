dec8eae7caff9b061c90a305b0a2350ce1b4c073
athenacats <124017569+athenacats@users.noreply.github.com>
2023-06-27 15:09:10 +0300

    Add css for profile page

--- frontend/src/app/components/pages/profile/profile.component.ts
+++ frontend/src/app/components/pages/profile/profile.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { OrderService } from 'src/app/services/order.service';
+
 import { UserService } from 'src/app/services/user.service';
 import { Order } from 'src/app/shared/models/order';
 import { User } from 'src/app/shared/models/user';
@@ -14,24 +13,9 @@ export class ProfileComponent implements OnInit {
   currentUser!: User;
   order!: Order;
 
-  constructor(
-    private userService: UserService,
-    private orderService: OrderService,
-    private activatedRoute: ActivatedRoute
-  ) {
-    /*const params = activatedRoute.snapshot.params;
-    if (!params['orderId']) return;
-    console.log(params['orderId']);
-    orderService.trackOrderById(params['orderId']).subscribe((order) => {
-      this.order = order;
-    });*/
-  }
+  constructor(private userService: UserService) {}
 
   ngOnInit(): void {
     this.currentUser = this.userService.currentUser;
-    if (this.currentUser.email == this.order.email) console.log(this.order.id);
-    /*console.log(this.order);
-    if (this.order) return;
-    this.orderService.getNewOrderForCurrentUser;*/
   }
 }
