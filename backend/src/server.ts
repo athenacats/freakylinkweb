6e6a86cb2e9b760b45735e01dd96af818fe538ee
athenacats <124017569+athenacats@users.noreply.github.com>
2023-07-09 14:12:44 +0300

    Add favicon & try fix cors issue

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:4200"],
+    origin: ["http://localhost:4200" || "https://thefreakylink.onrender.com"],
   })
 );
 
